97e6f824681a2f039709d5278949a610e0e3ec09
mateusahschulz <mateus.ahschulz@gmail.com>
2025-05-14 00:30:59 -0300

    inicial commit

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+
+export default defineConfig([
+  { ignores: ["dist/"] },
+  { files: ["src/**/*.{ts}"], plugins: { js }, extends: ["js/recommended"] },
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    languageOptions: { globals: globals.node },
+  },
+  ...tseslint.configs.recommended,
+  eslintPluginPrettierRecommended,
+]);
